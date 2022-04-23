b5943cba9306853615791b4c2f321966203e7e32
harryqas <harryqas94@gmail.com>
2022-04-24 01:44:49 +0900

    add vue, connect vue and spring

--- src/main/java/com/spring/vue/controller/BoardController.java
+++ src/main/java/com/spring/vue/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.spring.vue.service.BoardService;
 import com.spring.vue.vo.ResultVO;
 
+@CrossOrigin(origins = "*")
 @Controller
 @RequestMapping("board/")
 public class BoardController {
